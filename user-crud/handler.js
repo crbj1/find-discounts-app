bd70ec0ca1baf97a391db7b0bdf34a8738d34092
Chris Bergeron <chris.bergeron@ibm.com>
2023-02-08 16:08:11 -0600

    Added additional user CRUD functions

--- user-crud/handler.js
+++ user-crud/handler.js
@@ -44,3 +44,88 @@ module.exports.createUser = async (event, context, callback) => {
         callback(null, send(500, err.message));
     }
 };
+
+module.exports.updateUser = async (event, context, callback) => {
+    context.callbackWaitsForEmptyEventLoop = false;
+    let userId = event.pathParameters.id;
+    let data = JSON.parse(event.body);
+    try {
+        const params = {
+            TableName: USER_TABLE_NAME,
+            Key: { userId },
+            UpdateExpression: 'set #firstName = :firstName, #lastName = :lastName, #email = :email, #dateOfBirth = :dateOfBirth, #streetAddress1 = :streetAddress1, #streetAddress2 = :streetAddress2, #city = :city, #state = :state, #zipCode = :zipCode',
+            ExpressionAttributeNames: {
+                '#firstName': 'firstName',
+                '#lastName': 'lastName',
+                '#email': 'email',
+                '#dateOfBirth': 'dateOfBirth',
+                '#streetAddress1': 'streetAddress1',
+                '#streetAddress2': 'streetAddress2',
+                '#city': 'city',
+                '#state': 'state',
+                '#zipCode': 'zipCode'
+            },
+            ExpressionAttributeValues: {
+                ':firstName': data.firstName,
+                ':lastName': data.lastName,
+                ':email': data.email,
+                ':dateOfBirth': data.dateOfBirth,
+                ':streetAddress1': data.streetAddress1,
+                ':streetAddress2': data.streetAddress2,
+                ':city': data.city,
+                ':state': data.state,
+                ':zipCode': data.zipCode
+            },
+            ConditionExpression: 'attribute_exists(userId)'
+        };
+        await documentClient.update(params).promise();
+        callback(null, send(200, data));
+    } catch (err) {
+        callback(null, send(500, err.message));
+    }
+};
+
+module.exports.deleteUser = async (event, context, callback) => {
+    context.callbackWaitsForEmptyEventLoop = false;
+    let userId = event.pathParameters.id;
+    try {
+        const params = {
+            TableName: USER_TABLE_NAME,
+            Key: { userId },
+            ConditionExpression: 'attribute_exists(userId)'
+        };
+        await documentClient.delete(params).promise();
+        callback(null, send(200, userId));
+    } catch (err) {
+        callback(null, send(500, err.message));
+    }
+};
+
+module.exports.getUser = async (event, context, callback) => {
+    context.callbackWaitsForEmptyEventLoop = false;
+    let userId = event.pathParameters.id;
+    try {
+        const params = {
+            TableName: USER_TABLE_NAME,
+            Key: { userId },
+            ConditionExpression: 'attribute_exists(userId)'
+        };
+        const user = await documentClient.get(params).promise();
+        callback(null, send(200, user));
+    } catch (err) {
+        callback(null, send(500, err.message));
+    }
+};
+
+module.exports.getAllUsers = async (event, context, callback) => {
+    context.callbackWaitsForEmptyEventLoop = false;
+    try {
+        const params = {
+            TableName: USER_TABLE_NAME
+        };
+        const users = await documentClient.scan(params).promise();
+        callback(null, send(200, users));
+    } catch (err) {
+        callback(null, send(500, err.message));
+    }
+};
