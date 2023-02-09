634d557d476180604e8d9ea25efa4c3a0b42bd1a
Chris Bergeron <chris.bergeron@ibm.com>
2023-02-09 10:31:54 -0600

    Added basic implementation of location CRUD functions

---
+++ location-crud/handler.js
@@ -0,0 +1,110 @@
+'use strict';
+const DynamoDB = require("aws-sdk/clients/dynamodb");
+
+const documentClient = new DynamoDB.DocumentClient({
+    region: "us-east-1",
+    maxRetries: 3,
+    httpOptions: {
+        timeout: 5000
+    }
+});
+
+const LOCATION_TABLE_NAME = process.env.LOCATION_TABLE_NAME;
+
+const send = (statusCode, data) => {
+    return {
+        statusCode,
+        body: JSON.stringify(data)
+    };
+}
+
+module.exports.createLocation = async (event, context, callback) => {
+    context.callbackWaitsForEmptyEventLoop = false; //when callback is called, it is immediately executed
+    let data = JSON.parse(event.body);
+    try {
+        const params = {
+            TableName: LOCATION_TABLE_NAME,
+            Item: {
+                locationId: data.id,
+                name: data.name,
+                address: data.address
+            },
+            ConditionExpression: "attribute_not_exists(locationId)"
+        };
+        await documentClient.put(params).promise();
+        callback(null, send(201, data));
+    } catch (err) {
+        callback(null, send(500, err.message));
+    }
+};
+
+module.exports.updateLocation = async (event, context, callback) => {
+    context.callbackWaitsForEmptyEventLoop = false;
+    let locationId = event.pathParameters.id;
+    let data = JSON.parse(event.body);
+    try {
+        const params = {
+            TableName: LOCATION_TABLE_NAME,
+            Key: { locationId },
+            UpdateExpression: 'set #name = :name, #address = :address',
+            ExpressionAttributeNames: {
+                '#name': 'name',
+                '#address': 'address'
+            },
+            ExpressionAttributeValues: {
+                ':name': data.name,
+                ':address': data.address
+            },
+            ConditionExpression: 'attribute_exists(locationId)'
+        };
+        await documentClient.update(params).promise();
+        callback(null, send(200, data));
+    } catch (err) {
+        callback(null, send(500, err.message));
+    }
+};
+
+module.exports.deleteLocation = async (event, context, callback) => {
+    context.callbackWaitsForEmptyEventLoop = false;
+    let locationId = event.pathParameters.id;
+    try {
+        const params = {
+            TableName: LOCATION_TABLE_NAME,
+            Key: { locationId },
+            ConditionExpression: 'attribute_exists(locationId)'
+        };
+        await documentClient.delete(params).promise();
+        callback(null, send(200, locationId));
+    } catch (err) {
+        callback(null, send(500, err.message));
+    }
+};
+
+module.exports.getLocation = async (event, context, callback) => {
+    context.callbackWaitsForEmptyEventLoop = false;
+    let locationId = event.pathParameters.id;
+    try {
+        const params = {
+            TableName: LOCATION_TABLE_NAME,
+            Key: { locationId },
+            ConditionExpression: 'attribute_exists(locationId)'
+        };
+        const location = await documentClient.get(params).promise();
+        callback(null, send(200, location));
+    } catch (err) {
+        callback(null, send(500, err.message));
+    }
+};
+
+module.exports.getAllLocations = async (event, context, callback) => {
+    context.callbackWaitsForEmptyEventLoop = false;
+    try {
+        const params = {
+            TableName: LOCATION_TABLE_NAME
+        };
+        const locations = await documentClient.scan(params).promise();
+        callback(null, send(200, locations));
+    } catch (err) {
+        callback(null, send(500, err.message));
+    }
+};
