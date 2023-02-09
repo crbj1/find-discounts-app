28cdfde0245f9523508c0883bfeee6e30533e0b8
Chris Bergeron <chris.bergeron@ibm.com>
2023-02-09 11:27:05 -0600

    Added user pool, domain, client, and authorizer to template and implemented authorizer

---
+++ authorizer/authorizer.js
@@ -0,0 +1,46 @@
+const { CognitoJwtVerifier } = require("aws-jwt-verify");
+const COGNITO_USERPOOL_ID = process.env.COGNITO_USERPOOL_ID;
+const COGNITO_WEB_CLIENT_ID = process.env.COGNITO_WEB_CLIENT_ID;
+
+const jwtVerifier = CognitoJwtVerifier.create({
+    userPoolId: COGNITO_USERPOOL_ID,
+    tokenUse: "id",
+    clientId: COGNITO_WEB_CLIENT_ID
+});
+
+const generatePolicy = (principalId, effect, resource) => {
+    var tmp = resource.split(':');
+    var apiGatewayArnTmp = tmp[5].split('/');
+    var resource = tmp[0] + ":" + tmp[1] + ":" + tmp[2] + ":" + tmp[3] + ":" + tmp[4] + ":" + apiGatewayArnTmp[0] + '/*/*';
+    var authResponse = {};
+    authResponse.principalId = principalId;
+    if (effect && resource) {
+        let policyDocument = {
+            Version: "2012-10-17",
+            Statement: [
+                {
+                    Effect: effect,
+                    Resource: resource,
+                    Action: "execute-api:Invoke"
+                }
+            ]
+        };
+        authResponse.policyDocument = policyDocument;
+    }
+    authResponse.context = {
+        foo: "bar"
+    };
+    return authResponse;
+};
+
+exports.handler = async (event, context, callback) => {
+    //lambda authorizer code
+    var token = event.authorizationToken;
+    // Validate the token
+    try {
+        const payload = await jwtVerifier.verify(token);
+        callback(null, generatePolicy("user", "Allow", event.methodArn));
+    } catch (err) {
+        callback("Error: Invalid token");
+    }
+};
