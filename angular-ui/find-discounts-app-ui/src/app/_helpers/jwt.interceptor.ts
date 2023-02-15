42140de811179d805f58b7ace4c6f103c1c53780
Chris Bergeron <chris.bergeron@ibm.com>
2023-02-15 15:02:48 -0600

    Added CORS to template

--- angular-ui/find-discounts-app-ui/src/app/_helpers/jwt.interceptor.ts
+++ angular-ui/find-discounts-app-ui/src/app/_helpers/jwt.interceptor.ts
@@ -15,14 +15,20 @@ export class JwtInterceptor implements HttpInterceptor {
   constructor(private cognitoService: CognitoService) {}
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    const isAuthenticated = this.cognitoService.isAuthenticated();
-    if (isAuthenticated) {
-      request = request.clone({
-        setHeaders: {
-          Authorization: `Bearer ${this.cognitoService.getAccessToken}`
-        }
-      });
-    }
+    
+    this.cognitoService.isAuthenticated()
+    .then((isAuthenticated: boolean) => {
+      if (isAuthenticated) {
+        this.cognitoService.getAccessToken()
+        .then((token: string) => {
+          request = request.clone({
+            setHeaders: {
+              Authorization: `Bearer ${token}`
+            }
+          });
+        });
+      }
+    });
     
     return next.handle(request);
   }
